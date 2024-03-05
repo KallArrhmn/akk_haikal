c45eae49c29089457ebe83f4a4aed87576835ed2
KallArrhmn <kallchco@gmail.com>
2024-03-05 13:56:10 +0700

    first commit

---
+++ app/Http/Controllers/LikePhotoController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Photo;
+use App\Models\LikesPhoto;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LikePhotoController extends Controller
+{
+    public function like(Request $request)
+    {
+        $request->validate([
+            'photo_id' => ['required', 'exists:photos,id'],
+        ]);
+
+        if (!LikesPhoto::where('photo_id', $request->photo_id)->where('user_id', auth()->user()->id)->exists()) {
+            LikesPhoto::create([
+                'photo_id' => $request->photo_id,
+                'user_id' => auth()->user()->id
+            ]);
+        }
+
+        $data = Photo::with('user')
+            ->with('comments')
+            ->withCount('likes')
+            ->withExists('likedByUser', function ($query) {
+                $query->where('user_id', auth()->user()->id);
+            })
+            ->find($request->photo_id);
+
+        return response()->json(['like_count' => $data->likes_count]);
+    }
+    public function unlike(Request $request)
+    {
+        $request->validate([
+            'photo_id' => ['required', 'exists:photos,id'],
+        ]);
+
+        LikesPhoto::where('photo_id', $request->photo_id)->where('user_id', auth()->user()->id)->delete();
+        $data = Photo::with('user')
+            ->with('comments')
+            ->withCount('likes')
+            ->withExists('likedByUser', function ($query) {
+                $query->where('user_id', auth()->user()->id);
+            })
+            ->find($request->photo_id);
+
+
+        return response()->json(['like_count' => $data->likes_count]);
+    }
+
+}
